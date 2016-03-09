5a385d95937c10ed41083f40fb6535dbd1bc0486
Marcus Ray <makemymarc@aol.com>
2016-03-09 18:18:59 -0500

    Initial Project 1

---
+++ WebApplication1/Models/Form.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Form
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required()]
+        public string Name { get; set; }
+
+        [Required()]
+        public string Address { get; set; }
+
+        [Required()]
+        [DataType(DataType.PhoneNumber)]
+        public int Phone { get; set; }
+
+        [Required()]
+        [DataType(DataType.Date)]
+        public string Birthdate { get; set; }
+
+        public string Message { get; set; }
+    }
+}
